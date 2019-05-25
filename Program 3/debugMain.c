2e98417c52589ae97f079c727ca2f5d4317040f4
Chief <a>
2019-05-25 00:27:16 -0700

    upload file for testing on school server

--- Program 3/debugMain.c
+++ Program 3/debugMain.c
@@ -8,13 +8,16 @@
 #include <sys/wait.h>
 
 // function declarations
-char **getInputParsed();
+char **getInputParsed(char **, char **, int *);
+char *replace_str(char *, char *, char *);
 
 int main()
 {
-	int exitStatus = 0; // default to 0, set exit value if exited normally, or set termination signal
-	int howExited = 0;  // default to 0, set to 0 if exited normally, 1 if terminated
-
+	int foregroundExitStatus = 0; // default to 0, set exit value if exited normally, or set termination signal
+	int backgroundExitStatus = 0; // default to 0, set background exit value for printing
+	int howExited = 0;			  // default to 0, set to 0 if exited normally, 1 if terminated
+	int backgroundPIDS[100];
+	int pidCounter = 0;
 	while (1) // infinite loop, will exit by typing 'exit' in prompt
 	{
 
@@ -22,14 +25,21 @@ int main()
 		char **parsedString = NULL;
 		char *dirName = NULL;
 		pid_t spawnpid = -5;
-		int childExitMethod;
+		int childExitMethod = -5;
+
+		int inputFD = -1; // each while loop, these variables get initialized to NULL
+		int outputFD = -1;
+		char *inputFile = NULL;
+		char *outputFile = NULL;
+		int backgroundFlag = 0; // default 0, set to 1 to execute as background process
 
 		// print prompt for command:
 		printf(": ");
 		fflush(stdout);
-		parsedString = getInputParsed();
+		parsedString = getInputParsed(&inputFile, &outputFile, &backgroundFlag);
 		if (parsedString[0] != NULL) // ignore empty input
 		{
+			// *******************************START OF BUILT-IN COMMANDS***************************************
 			// check if input was a built - in shell command : cd, status, exit int idx = 0;
 			if (strcmp(parsedString[0], "cd") == 0)
 			{
@@ -64,16 +74,17 @@ int main()
 				// print exit value 0 if status called before a foreground process
 				if (howExited == 0)
 				{ // exited normally, print exit value
-					printf("exit value %d\n", exitStatus);
+					printf("exit value %d\n", foregroundExitStatus);
 					fflush(stdout); // test print
 				}
 				if (howExited == 1)
 				{ // exited by termination, print termination signal
-					printf("terminated by signal %d\n", exitStatus);
+					printf("terminated by signal %d\n", foregroundExitStatus);
 					fflush(stdout); // test print
 				}
-				// printf("exit value %d\n", exitStatus);fflush(stdout);	// test print
+				// printf("exit value %d\n", foregroundExitStatus);fflush(stdout);	// test print
 			}
+			// *******************************END OF BUILT-IN COMMANDS*****************************************
 			else
 			{
 				// if this point is reached, that means it is not a built-in command. fork a child and execute given command
@@ -85,27 +96,134 @@ int main()
 					exit(10);
 					break;
 				case 0:
-					// child process
-					// exec here
-					execvp(parsedString[0], parsedString);
-					// exit here, if exec succeeds, will not reach this line
-					exit(11);
-					break;
-				default:
-					waitpid(spawnpid, &childExitMethod, 0); // waits for it's child to terminate, store exit status in 2nd arg
-					// checking exit status of child
-					if (WIFEXITED(childExitMethod) != 0)
-					{ // child terminated normally, get exit value
-						exitStatus = WEXITSTATUS(childExitMethod);
-						howExited = 0;
+					// ***********************START OF CHILD PROCESS*******************************************
+					// redirect input and output, if applicable.
+					if (inputFile != NULL)
+					{ // inputFile is default NULL, if not NULL then input redirection was specified
+						// try to open file with same name as inputFile, if successful then redirect FD = 0 to where FD = inputFile points
+						inputFD = open(inputFile, O_RDONLY);
+						if (inputFD == -1)
+						{
+							// error opening file
+							printf("Unable to open input file \"%s\"", inputFile);
+							fflush(stdout);
+							exit(1);
+						}
+						else
+						{
+							dup2(inputFD, 0); // redirect stdin to where inputFD points to
+						}
 					}
 					else
 					{
-						// child terminated by signal
-						exitStatus = WTERMSIG(childExitMethod);	// get termination signal
-						howExited = 1;							// set status flag
+						// if inputFile not specified, and background flag is set, then have stdin redirect from /dev/null
+						if (backgroundFlag == 1)
+						{
+							// open /dev/null
+							int devNull = open("/dev/null", O_RDONLY);
+							if (devNull == -1)
+							{
+								// error
+								printf("Unable to open /dev/null\n");
+								fflush(stdout);
+								exit(1);
+							}
+							else
+							{
+								dup2(devNull, 0);
+							}
+						}
+					}
+					if (outputFile != NULL)
+					{
+						// outputFile is default NULL, if not NULL, then output redirection was specified
+						outputFD = open(outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0744); // open file for as truncate; clear file for writing if exists, or create if doesn't exist
+						if (outputFD == -1)
+						{
+							// error opening file
+							printf("Unable to open output file \"%s\"", outputFile);
+							fflush(stdout);
+							exit(1);
+						}
+						else
+						{
+							dup2(outputFD, 1); // redirect stdout to where outputFD points to
+						}
+					}
+					else
+					{
+						// if inputFile not specified, and background flag is set, then have stdin redirect from /dev/null
+						if (backgroundFlag == 1)
+						{
+							// open /dev/null
+							int devNull = open("/dev/null", O_WRONLY);
+							if (devNull == -1)
+							{
+								// error
+								printf("Unable to open /dev/null\n");
+								fflush(stdout);
+								exit(1);
+							}
+							else
+							{
+								dup2(devNull, 1);
+							}
+						}
+					}
+					// exec here
+					if (execvp(parsedString[0], parsedString) == -1)
+					{
+						// if reached here, then there is an error, set foregroundExitStatus to 1 and print error to screen
+						foregroundExitStatus = 1;
+						printf("There was an error executing that command, please try again\n");
+						fflush(stdout);
+						exit(1);
 					}
 					break;
+					// ***********************END OF CHILD PROCESS*********************************************
+
+				default:
+					// ***********************START OF PARENT PROCESS******************************************
+
+					// determine if parent will wait for foreground child or NOT WAIT for background child
+					if (backgroundFlag == 0) // backgrond flag NOT SET, wait for foreground child
+					{
+						spawnpid = waitpid(spawnpid, &childExitMethod, 0); // waits for it's child to terminate, store exit status in 2nd arg
+						if (spawnpid == -1)
+						{
+							printf("waitpid() error\n");
+							exit(1);
+							break;
+						}
+						// checking exit status of child
+						else
+						{
+							if (WIFEXITED(childExitMethod) != 0)
+							{ // child terminated normally, get exit value
+								foregroundExitStatus = WEXITSTATUS(childExitMethod);
+								howExited = 0;
+							}
+							else
+							{
+								// child terminated by signal
+								foregroundExitStatus = WTERMSIG(childExitMethod); // get termination signal
+								howExited = 1;									  // set status flag
+							}
+							break;
+						}
+					}
+					// background flag SET, don't wait for background child
+					else
+					{
+						spawnpid = waitpid(spawnpid, &childExitMethod, WNOHANG);
+						if (spawnpid == -1)
+						{
+							printf("waitpid() error\n");
+							exit(1);
+						}
+						break;
+					}
+					// ***********************END OF PARENT PROCESS********************************************
 				}
 			}
 			// free(userInput);
@@ -118,7 +236,7 @@ int main()
 }
 
 // This function gets input from stdin, parses the string, and then returns an array of pointers with the separated arguments
-char **getInputParsed()
+char **getInputParsed(char **inputFile, char **outputFile, int *backgroundFlag)
 {
 	// getline test
 	// read input from stdin, then parse and print each word on new line
@@ -128,7 +246,7 @@ char **getInputParsed()
 	// store tokens in this string array
 	char **parsedString = malloc(512 * sizeof(char));
 
-	if ((numCharRead = getline(&buffer, &buffer_size, stdin)) == -1)
+	if ((numCharRead = getline(&buffer, &buffer_size, stdin)) == -1) // read in line from user, check if read correctly
 	{
 		perror("error with getline");
 	}
@@ -137,7 +255,7 @@ char **getInputParsed()
 	buffer[numCharRead - 1] = '\0';
 
 	// printf("the line read in was: \"%s\".\n", buffer);
-	fflush(stdout); // test print this line
+	// fflush(stdout); // test print this line
 
 	// use strtok to parse string
 	char *token = strtok(buffer, " ");
@@ -146,11 +264,89 @@ char **getInputParsed()
 	int counter = 0;
 	while (token != NULL)
 	{
-		parsedString[counter] = token;
-		counter++;
-		token = strtok(NULL, " ");
+		if (strcmp(token, "<") == 0)
+		{
+			// if input redirection found
+			// then the next token is the input file name
+			token = strtok(NULL, " ");
+			// search token for "$$", if found, replace with process id
+			if ((strstr(token, "$$")) != NULL)
+			{
+				int thisPid = getpid();
+				char pidstr[16];
+				sprintf(pidstr, "%d", thisPid);
+				token = replace_str(token, "$$", pidstr);
+			}
+			// allocate memory and copy input file name over
+			*inputFile = calloc(512, sizeof(char));
+			strcpy(*inputFile, token);
+			// prepare next token to be examined
+			token = strtok(NULL, " ");
+		}
+		else if (strcmp(token, ">") == 0)
+		{ // else if output redirection found
+			// then the next token is the output file name
+			token = strtok(NULL, " ");
+			// search token for "$$", if found, replace with process id
+			if ((strstr(token, "$$")) != NULL)
+			{
+				int thisPid = getpid();
+				char pidstr[16];
+				sprintf(pidstr, "%d", thisPid);
+				token = replace_str(token, "$$", pidstr);
+			}
+			// allocate memory and copy output file name over
+			*outputFile = calloc(512, sizeof(char));
+			strcpy(*outputFile, token);
+			// prepare next token to be examined
+			token = strtok(NULL, " ");
+		}
+		else if (strcmp(token, "&") == 0)
+		{						 // else if reached & at end of command
+			*backgroundFlag = 1; // child will check flag to see if process will be run in background
+			break;				 // & as a token will always be at end of command list, break from loop
+		}
+		else
+		{
+			// if reached this point, then has to be command or argument
+			// search token for "$$", if found, replace with process id
+			if ((strstr(token, "$$")) != NULL)
+			{
+				int thisPid = getpid();
+				char pidstr[16];
+				sprintf(pidstr, "%d", thisPid);
+				token = replace_str(token, "$$", pidstr);
+			}
+			// store token in parsed string
+			parsedString[counter] = token;
+			// if line starts with #, stop parsing and function returns pointer to null
+			if (((int)(*parsedString[0])) == '#')
+			{
+				break;
+			}
+			counter++;
+			token = strtok(NULL, " ");
+		}
 	}
+	// make sure parsed string ends with NULL
 	parsedString[counter] = NULL;
-
 	return parsedString;
+}
+
+// function to find '$$' in substring and replace with passed in pid in string
+// https://www.linuxquestions.org/questions/programming-9/replace-a-substring-with-another-string-in-c-170076/
+char *replace_str(char *str, char *orig, char *rep)
+{
+	static char buffer[4096];
+	char *p;
+
+	if (!(p = strstr(str, orig))) // Is 'orig' even in 'str'?
+		return str;
+
+	strncpy(buffer, str, p - str); // Copy characters from 'str' start to 'orig' st$
+	buffer[p - str] = '\0';
+
+	sprintf(buffer + (p - str), "%s%s", rep, p + strlen(orig));
+
+	return buffer;
 }
